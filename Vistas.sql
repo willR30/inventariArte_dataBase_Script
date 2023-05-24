cc70e1a04b92bbe3e93fba54fb07e676b24d22f9
ZharoonT <Zharoont@gmail.com>
2023-05-24 16:33:34 -0600

    Delete unused Scripts

--- Vistas.sql
+++
@@ -1,9 +0,0 @@
-/*--------------------------------------------------VISTAS------------------------------------------------------------------------------*/
-CREATE VIEW CategoryView AS
-SELECT id, name,description
-FROM Category;
-
-CREATE VIEW BillStateView AS
-SELECT id, name
-FROM Bill_State;
-
